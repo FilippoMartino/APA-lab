dcbd10851b35f9d1671fd9e66396d6187fa6fab4
Filippo Martino <filippo.pagge@gmail.com>
2020-10-29 10:57:06 +0100

    Update main.c
    aggiunta dei casi mancanti, ancora necessario testarlo un pò più aggressivamente e di essere commentato.
    Ho oltretutto aggiornato la funzione per la lettura di metacaratteri con il '.', precesiamente nel caso [***].[***], ho cambiato la gestione dalla strtok (che dava problemi di accesso alla memoria) con una più sicura memmove

--- LAB04/E03/main.c
+++ LAB04/E03/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAX_STRING_SIZE 100
 #define MAX_REGEX_SIZE 20
@@ -20,7 +21,7 @@ char *single_not_upper_char(char *, char *);
 char *single_upper_char(char *, char *);
 
 void clear_buffer(char *);
-
+void fragment_regex(char*, char*, char*, char*, char*);
 
 
 int main(int argc, const char * argv[]) {
@@ -43,31 +44,37 @@ int main(int argc, const char * argv[]) {
 	return 0;
 }
 
-
-
 char *cercaRegexp(char *src, char *regexp){
-	
-	
 	
 	/* Gestisco il caso di regex con metacarattere '.' */
 	if (strstr(regexp, ".") != NULL)
 		return single_char_generic(src, regexp);
 	
 	/* Gestisco regex con metacarattere '[...]' */
-	if (strstr(regexp, "[") && strstr(regexp, "]")) {
+	if (strstr(regexp, "[") && strstr(regexp, "]") && !strstr(regexp, "[^")) {
 		return single_char(src, regexp);
 	}
 	
+	/* Gestisco regex con metacarattere [^...]*/
+	if (strstr(regexp, "[^") && strstr(regexp, "]")) {
+		return single_not_char(src, regexp);
+	}
+	
+	if (strstr(regexp, "\\a")) {
+		return single_not_upper_char(src, regexp);
+	}
+	
+	if (strstr(regexp, "\\A")) {
+		return single_upper_char(src, regexp);
+	}
 	
 	return generic_sintrg(src, regexp);
 	
 }
 
-
 char *generic_sintrg(char *src, char *regexp){
 	return strstr(src, regexp);
 }
-
 
 /**
  Nelle regex il '.' inserito all'interno di una stringa implica la ricerca in un testo
@@ -115,19 +122,28 @@ char *single_char_generic(char *src, char *regexp){
 	if (regexp[i] == '.')
 		metachar = i;
 	
+	char before[MAX_REGEX_SIZE];
+	char after[MAX_REGEX_SIZE];
+
 	
-	char *tok = strtok(regexp, ".");
-	char *tok_1 = strdup(tok);
-	char *temp = strdup(src);
-	char *p = strstr(temp, tok);
-	p[1] = '1';
+	memmove(before, regexp, metachar);
+	memmove(after, regexp + metachar + 1, strlen(regexp) - strlen(before));
 	
-	tok = strtok(NULL, ".");
 	
-	if (p != NULL && strstr(temp, tok) != NULL)
-		return strstr(src, tok_1);
-	else
-		return NULL;
+	
+	char *p_1 = strstr(src, before);
+	if (p_1 == NULL) return NULL;
+	
+	char temp = src[strlen(src) - strlen(p_1) + 1];
+	src[strlen(src) - strlen(p_1) + 1] = '.';
+	
+	
+	char *p_2 = strstr(src, after);
+	src[strlen(src) - strlen(p_1) + 1] = temp;
+	
+	if (p_2 == NULL) return NULL;
+	
+	return p_1;
 	
 	
 }
@@ -143,42 +159,7 @@ char *single_char(char *src, char *regexp){
 	char meta[MAX_REGEX_SIZE];
 	char after_meta[MAX_REGEX_SIZE];
 	
-	clear_buffer(before_meta);
-	clear_buffer(meta);
-	clear_buffer(after_meta);
-	
-	int is_meta_writed = 0;
-	int is_meta_being_writed = 0;
-	int is_before_null = 0;
-	int is_after_null = 0;
-	
-	if (regexp[0] == '[')
-		is_before_null = 1;
-	
-	if (regexp[strlen(regexp) - 1] == ']')
-		is_after_null = 1;
-	
-	for (int i = 0; i < strlen(regexp); i++) {
-	
-		if(regexp[i] != '[' && !is_meta_being_writed && !is_meta_writed && !is_before_null)
-			sprintf(before_meta, "%s%c", before_meta, regexp[i]);
-		
-		if (regexp[i] == '[')
-			is_meta_being_writed = 1;
-		
-		if (is_meta_being_writed && regexp[i] != '[' && regexp[i] != ']') {
-			sprintf(meta, "%s%c", meta, regexp[i]);
-		}
-		
-		if (regexp[i] == ']'){
-			is_meta_being_writed = 0;
-			is_meta_writed = 1;
-		}
-		
-		if (!is_meta_being_writed && is_meta_writed && regexp[i] != ']' && !is_after_null)
-			sprintf(after_meta, "%s%c", after_meta, regexp[i]);
-		
-	}
+	fragment_regex(before_meta, meta, after_meta, regexp, "[]");
 	
 	char my_regex[MAX_STRING_SIZE];
 	
@@ -193,14 +174,131 @@ char *single_char(char *src, char *regexp){
 	return NULL;
 }
 
-//char *single_not_char(char *src, char *regexp);
+char *single_not_char(char *src, char *regexp){
+	
+	/* Caso [^] vuoto da solo */
+	if (strlen(regexp) == 3 && strstr(regexp, "[^]")) {
+		return src;
+	}
+	
+	char before_meta[MAX_REGEX_SIZE];
+	char meta[MAX_REGEX_SIZE];
+	char after_meta[MAX_REGEX_SIZE];
+	
+	fragment_regex(before_meta, meta, after_meta, regexp, "[^]");
+	
+	char my_regex[MAX_STRING_SIZE];
+	
+	for (int i = 0; i < strlen(meta); i++) {
+		sprintf(my_regex, "%s%c%s", before_meta, meta[i], after_meta);
+		char* p = strstr(src, my_regex);
+		if (p == NULL){
+			sprintf(my_regex, "%s%c%s", before_meta, '.', after_meta);
+			/* Togliendo il carattere che non deve esserci, lo sostisco con il . e passo alla funzione di ricerca generica */
+			return single_char_generic(src, my_regex);
+		}
+	}
+	
+	/* Se siamo qui è perchè non abbiamo trovato ricorrenze, ritorno NULL */
+	return NULL;
+}
 
-//char *single_not_upper_char(char *src, char *regexp);
+char *single_not_upper_char(char *src, char *regexp){
+	
+	int pos = 0;
+	
+	if (regexp[0] == '\\') {
+		regexp = regexp + 1;
+		regexp[0] = '.';
+		pos = 0;
+	} else {
+		
+		char *p = strstr(regexp, "\\a");
+		unsigned long start_point = strlen(regexp) - strlen(p);
+		
+		memmove(regexp + start_point, p + 2, strlen(p));
+		regexp[start_point] = '.';
+		pos = (int) start_point;
+	}
+	
+	char* p = single_char_generic(src, regexp);
 
-//char *single_upper_char(char *src, char *regexp);
+	if (!isupper(p[pos])) return p;
+
+	return NULL;
+}
+
+char *single_upper_char(char *src, char *regexp){
+	
+	int pos = 0;
+	
+	if (regexp[0] == '\\') {
+		regexp = regexp + 1;
+		regexp[0] = '.';
+		pos = 0;
+	} else {
+		
+		char *p = strstr(regexp, "\\A");
+		unsigned long start_point = strlen(regexp) - strlen(p);
+		
+		memmove(regexp + start_point, p + 2, strlen(p));
+		regexp[start_point] = '.';
+		pos = (int) start_point;
+	}
+	
+	char* p = single_char_generic(src, regexp);
+
+	if (isupper(p[pos])) return p;
+
+	return NULL;
+}
 
 void clear_buffer(char * buffer){
 	for (int i = 0; i < strlen(buffer); i++) {
 		buffer[i] = 0;
 	}
+}
+
+void fragment_regex(char* before, char* meta, char* after, char* regex, char* regex_type){
+	
+	clear_buffer(before);
+	clear_buffer(meta);
+	clear_buffer(after);
+	
+	int is_meta_writed = 0;
+	int is_meta_being_writed = 0;
+	int is_before_null = 0;
+	int is_after_null = 0;
+	
+	if (regex[0] == '[')
+		is_before_null = 1;
+	
+	if (regex[strlen(regex) - 1] == ']')
+		is_after_null = 1;
+	
+	for (int i = 0; i < strlen(regex); i++) {
+		
+		if(regex[i] != '[' && !is_meta_being_writed && !is_meta_writed && !is_before_null)
+			sprintf(before, "%s%c", before, regex[i]);
+		
+		if (regex[i] == '[')
+			is_meta_being_writed = 1;
+		
+		if (is_meta_being_writed && regex[i] != '[' && regex[i] != ']') {
+			sprintf(meta, "%s%c", meta, regex[i]);
+		}
+		
+		if (regex[i] == ']'){
+			is_meta_being_writed = 0;
+			is_meta_writed = 1;
+		}
+		
+		if (!is_meta_being_writed && is_meta_writed && regex[i] != ']' && !is_after_null)
+			sprintf(after, "%s%c", after, regex[i]);
+		
+	}
+	
+	/* Nel caso in cui il tipo sia [^] devo togliere il primo carattere di meta '^' appunto*/
+	if (!strcmp(regex_type, "[^]")) meta = meta + 1;
+	
 }
