fb21b74505bb994fa337a5434178cb8f0485d492
Filippo Martino <filippo.pagge@gmail.com>
2020-12-11 14:17:20 +0100

    Aggiornamento codice da algoritmo xcode

--- LAB08/E3/main.c
+++ LAB08/E3/main.c
@@ -101,7 +101,9 @@ void free_players(Player*);
 void print_equipment(Item);
 void print_equipment_list(Equipment*);
 void free_equipment(Equipment*);
-
+void init_stat(Stat*);
+Stat sum_stat(Stat, Stat);
+	
 int main(int argc, const char * argv[]) {
 	
 	Player* my_players = NULL;
@@ -119,16 +121,14 @@ int main(int argc, const char * argv[]) {
 				break;
 			case add_eq_to_player: equip_player(&my_equipments, &my_players);
 				break;
-			case get_stat: get_player_stat((my_players);
+			case get_stat: get_player_stat(my_players);
 				break;
 			case end:
 				break;
 		}
 	}
 	
-	print_players(my_players);
-	print_equipment_list(my_equipments);
-	
+	/* Libero lo spazio allocato per le due liste */
 	free_players(my_players);
 	free_equipment(my_equipments);
 	
@@ -202,13 +202,14 @@ void add_player_by_console(Player** players){
 	for (; temp->next != NULL; temp = temp->next);
 	
 	temp->next = new_player(id, player_name, class, player_stat);
-	
-	
 }
 
+/**
+ Cancella un personaggio dalla lista in base all'id (specificato da console)
+ 
+ @param players puntatore a puntatore alla testa della lista dei personaggi
+ */
 void delete_player_by_id(Player** players){
-	
-	
 	/* Gesitone lista vuota */
 	if (players == NULL)
 		return;
@@ -332,8 +333,6 @@ Player* new_player(char id[ID_SIZE], char name[MAX_STRING], char class[MAX_STRIN
 	new->next = NULL;
 	
 	return new;
-	
-	
 }
 
 /**
@@ -363,6 +362,12 @@ void add_equipments(Equipment** eq){
 	
 }
 
+/**
+ Assegna ad un giocatore (se possibile) un oggetto dalla lsita degli items disponibili
+ 
+ @param eq puntatore a puntatore alla testa della lista degli equipaggiamenti
+ @param players puntatore a puntatore alla testa della lista dei personaggi
+ */
 void equip_player(Equipment** eq, Player** players){
 	
 	if (*players == NULL) {
@@ -400,11 +405,12 @@ void equip_player(Equipment** eq, Player** players){
 					print_equipment(*my_eq->item);
 					printf("Oggetto inserito con successo nell'inventario.\n");
 					return;
-				} else {
-					printf("Non è stato trovato nessun equipaggiamento corrsipondente al codice inserito\n");
-					return;
 				}
+				
 			}
+			
+			printf("Non sono stati trovati oggetti con l'id specificato.\n");
+			return;
 			
 		}
 	}
@@ -413,12 +419,71 @@ void equip_player(Equipment** eq, Player** players){
 	
 }
 
+/**
+ Stampa le specifiche di un giocatore (la scelta viene fatta fare dalla funzione mediante inseirmento dell'id) calcolate ponderate in
+ base agli elementi presenti nell'inventario
+ 
+ @param players puntatore alla testa della lista dei giocatori
+ */
 void get_player_stat(Player* players){
 	
 	char id[MAX_STRING];
+	printf("Inserire id del personaggio per il quale calcolare le statistiche: ");
+	scanf("%s", id);
+	Stat total_stat;
+	init_stat(&total_stat);
+    
+	for (; players->next != NULL; players = players->next) {
+		
+		if (!strcmp(players->bio->id, id)) {
+			
+			if (players->bio->item_number == 0) {
+				printf("Il personaggio che hai scelto non ha elementi nell'inventario.\n");
+				return;
+			}
+			
+			/* Calcolo delle statistiche totale degli oggetti nell'invetario */
+			for (int i = 0; i < players->bio->item_number; i++) {
 				
+				total_stat = sum_stat(total_stat, players->bio->player_inv[i].eq_stat);
+
+			}
+			
+			/* Aggiunta delle statistiche proprie del personaggio */
+			total_stat = sum_stat(total_stat, players->bio->player_stat);
+			
+			/* Controllo elementi negativi */
+			
+			if (total_stat.atk < 0) total_stat.atk = 0;
+			if (total_stat.def < 0) total_stat.def = 0;
+			if (total_stat.hp < 0) total_stat.hp = 0;
+			if (total_stat.mag < 0) total_stat.mag = 0;
+			if (total_stat.mp < 0) total_stat.mp = 0;
+			if (total_stat.spr < 0) total_stat.spr = 0;
+			
+			printf("Stampa del personaggio con statistiche dell'inventario applicate:\n");
+			printf("\nId: %s\nNome: %s\nClasse: %s\nhp: %d\nmp: %d\natk: %d\ndef: %d\nmag: %d\nspr: %d\n",
+				   players->bio->player_name,
+				   players->bio->player_name,
+				   players->bio->class,
+				   total_stat.hp,
+				   total_stat.mp,
+				   total_stat.atk,
+				   total_stat.def,
+				   total_stat.mag,
+				   total_stat.spr
+				   );
+			
+			return;
+
+		}
+	}
+	
+	printf("Non sono stati trovati personaggi corrispondeti all'id inserito\n");
+	return;
+	
 }
-									
+			
 /**
  Genera una lista di "equipaggiamenti"
  
@@ -659,4 +724,40 @@ void free_equipment(Equipment* eq){
 		free(temp->item);
 		free(temp);
 	}
+}
+
+/**
+ Inizializza a 0 i parametri di una struttura @b Stat
+ 
+ @param stat puntatore alla struttura da inizializzare
+ */
+void init_stat(Stat* stat){
+	stat->atk = 0;
+	stat->def = 0;
+	stat->hp = 0;
+	stat->mag = 0;
+	stat->mp = 0;
+	stat->spr = 0;
+}
+
+/**
+ Somma due strutture Stat
+ 
+ @param stat_1 la prima struttura
+ @param stat_2 seconda struttura
+ @returns @b Stat una struttura con parametri la somma delle due passate
+ */
+Stat sum_stat(Stat stat_1, Stat stat_2){
+	Stat sum;
+	init_stat(&sum);
+	
+	sum.hp = stat_1.hp + stat_2.hp;
+	sum.atk = stat_1.atk + stat_2.atk;
+	sum.def = stat_1.def + stat_2.def;
+	sum.mag = stat_1.mag + stat_2.mag;
+	sum.mp = stat_1.mp + stat_2.mp;
+	sum.spr = stat_1.spr + stat_2.spr;
+	
+	return sum;
+	
 }
